ed8bf17c4430acf3afd0d0959b4fcc2ada3fa955
Jon Atkinson <JonnyA@gmail.com>
2011-09-11 22:12:44 -0700

    Manually fixed Firefox's inability to see that the esi tags are self-closed.

--- src/chrome/content/browserOverlay.js
+++ src/chrome/content/browserOverlay.js
@@ -14,14 +14,24 @@ EsiProcessor.BrowserOverlay =  // class
             if (freshDoc.domain != 'jonatkinson.home.mindspring.com')  { return; }
 
             var esiTags = freshDoc.getElementsByTagName("esi:include");
-            var esiRequests = new Array(esiTags.length);
+            //FIXME: fix the odd nested behavior of esi tags in the DOM.
+            // things to try: 1) confirm that Firfox does this for this tag w/o this js code.
+            // if so, research why. dos it do it with block tags and inline tags? (<div />)
+            // does it do it with any tag that it doesnt know specifically can be a single tag?
+            // 2) double-check there isn't a bug w/ the use of esiTags.length or anything else live
+            // 3) try setting up a non-live nodelist for use by the main XHR code
+            // 4) try removing the esi tag entirely and replacing it with the results
+            // if any children, move them up. perhaps go in reverse order for this.
+            // try just moving the children up in reverse order.
 
-            for (var i = 0; i < esiTags.length; i++)
+            var esiRequests = new Array(esiTags.length);
+            for (var i = esiTags.length -1; i >= 0; i--)
             {
                 esiRequests[i] = new XMLHttpRequest();
                 esiRequests[i].open('GET', esiTags[i].getAttribute('src'), true);
-                
+
                 let j = i;
+
                 esiRequests[i].onreadystatechange = function(event) {
                     if (this.readyState != 4)  { return; }
 
@@ -34,17 +44,21 @@ EsiProcessor.BrowserOverlay =  // class
                     let esiContentElement = freshDoc.createElement("span");
                     esiContentElement.id = "esi_processor-" + j;
                     esiContentElement.innerHTML = esiContent;
-                    // FIXME: Add the content as a direct child of the ESI tag.
-                    // Or as the next sibling of the ESI tag, if it is a node itself.
-                    //esiTags[j].insertBefore(esiContentElement, null);
-                    esiTags[j].appendChild(esiContentElement);
-                    //esiTags[j].parentNode.insertBefore(esiContentElement, esiTags[j].nextSibling);
+                    //esiTags[j].appendChild(esiContentElement);
+                    esiTags[j].parentNode.insertBefore(esiContentElement, esiTags[j]);
+                    
+                    while (esiTags[j].hasChildNodes())
+                    {
+                        esiTags[j].parentNode.insertBefore(esiTags[j].childNodes[0], esiTags[j]);
+                    }
+
+                    esiTags[j].parentNode.removeChild(esiTags[j]);
                 }
                 esiRequests[i].send(null);
             }
 
             // Components.utils.reportError("Done!");
-            
+
         // FIXME: remove handler now? or needed for reloads?
         }
     }
