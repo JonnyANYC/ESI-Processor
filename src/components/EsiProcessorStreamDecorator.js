de9aabc27f32a509d08ead348ecae7536f7126d6
JonnyA <devnull@localhost>
2013-04-04 23:53:46 -0700

    * Added Apache license notification to all JS source files.

--- src/components/EsiProcessorStreamDecorator.js
+++ src/components/EsiProcessorStreamDecorator.js
@@ -1,3 +1,20 @@
+﻿/*
+ *   Copyright 2013 Jonathan Atkinson
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+
 // Helper functions
 if (typeof Cc == "undefined") {
     var Cc = Components.classes;
@@ -290,6 +307,3 @@ if ("generateNSGetFactory" in XPCOMUtils)
   var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);  // Firefox 4.0 and higher
 else
   var NSGetModule = XPCOMUtils.generateNSGetModule(components);    // Firefox 3.x
-
-
-
